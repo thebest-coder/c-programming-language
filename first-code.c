c834d3958905a0ff254da488686aec7350071abe
thebest-coder <abdurasulovjavokhir0304@gmail.com>
2022-07-16 13:31:43 +0500

    c programming language

---
+++ first-code.c
@@ -0,0 +1,31 @@
+#include <stdio.h> // standart input output header
+#include <stdlib.h> // standart library header
+
+int main() // asosiy funksiya
+{
+    printf("Hello World!\n\n"); // printf function
+    printf("Hi Javokhir\n\n");
+    printf("      C   C\n");
+    printf("   C        C\n");
+    printf("  C\n");
+    printf(" C\n");
+    printf(" C              Programming  Language\n");
+    printf(" C\n");
+    printf("  C\n");
+    printf("   C        C\n");
+    printf("     C   C\n");
+
+    // /n -> new line
+
+    return 0; // dastur muaffaqiyatli ishlashni bildiradi
+}
+
+// #   -> sharp
+// <>  -> diamon paranthes
+// ()  -> paranthes
+// []  -> brackets
+// {}  -> curly brackets
+// /   -> slash
+// \   -> back slash
+// "   -> quote
+// '   -> bir tirnoq
